d432d738c156a209e65aad491c090beb3005ace2
iamdeyg <deygderin@gmail.com>
2025-04-23 22:10:38 +0100

    made changes to mongodb url

--- config.js
+++ config.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/birthday_db", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://deygderin:Adedeji1997@cluster0.pmjvhod.mongodb.net/birthday-app",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB Connected successfully ✅");
   } catch (err) {
     console.error(err);
